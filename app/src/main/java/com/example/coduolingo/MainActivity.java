2af7538a73793f06e91eadd319536f6e48bb9977
bobyo <bobyourhumanhelper@gmail.com>
2020-03-29 15:48:32 +0300

    yakut

--- app/src/main/java/com/example/coduolingo/MainActivity.java
+++ app/src/main/java/com/example/coduolingo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.coduolingo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -18,15 +19,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     TextView qs;
-    EditText inp;
+    Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String lesson = DownloadReadlessons.downloadlesson("57933", MainActivity.this); // download lesson by ID
         Log.d("gojo", "h"); // pro vi estas gojo
-        HashMap<String, String> hashMap = DownloadReadlessons.readqs("57933", "Math", "1", MainActivity.this); // read qs by ID + name + question number
+        final HashMap<String, String> hashMap = DownloadReadlessons.readqs("57933", "Math", "1", MainActivity.this); // read qs by ID + name + question number
         qs = (TextView)findViewById(R.id.textView);
         qs.setText(hashMap.get("qs"));
         Log.d("gojo", hashMap.get("qs"));
+        submit = (Button) findViewById(R.id.button);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText inp = (EditText) findViewById(R.id.inp);
+                String ans = inp.getText().toString();
+                boolean h=hashMap.get("Answer")==ans;
+                if (ans.equals(hashMap.get("Answer"))){
+                    qs.setText("Guten");
+                }
+            }
+        });
+
+
     }}
