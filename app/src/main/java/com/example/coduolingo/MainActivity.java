db3ac4cd329dddeaff6d10e39eef192d9a90a447
bobyo <bobyourhumanhelper@gmail.com>
2020-03-29 17:30:45 +0300

    yakut

--- app/src/main/java/com/example/coduolingo/MainActivity.java
+++ app/src/main/java/com/example/coduolingo/MainActivity.java
@@ -20,19 +20,21 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     TextView qs;
     Button submit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadquestion("57933","Math", "1");
+        loadquestion("57933", "Math", "20");
 
 
     }
-    public void loadquestion(String id, String name, String qs_num){
+
+    public void loadquestion(String id, String name, String qs_num) {
         String lesson = DownloadReadlessons.downloadlesson(id, MainActivity.this); // download lesson by ID
         Log.d("gojo", "h"); // pro vi estas gojo
         final HashMap<String, String> hashMap = DownloadReadlessons.readqs(id, name, qs_num, MainActivity.this); // read qs by ID + name + question number
-        qs = (TextView)findViewById(R.id.textView);
+        qs = (TextView) findViewById(R.id.textView);
         qs.setText(hashMap.get("qs"));
         Log.d("gojo", hashMap.get("qs"));
         submit = (Button) findViewById(R.id.button);
@@ -41,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText inp = (EditText) findViewById(R.id.inp);
                 String ans = inp.getText().toString();
-                if (ans.equals(hashMap.get("Answer"))){
+                if (ans.equals(hashMap.get("Answer"))) {
                     qs.setText("Guten");
                 }
             }
