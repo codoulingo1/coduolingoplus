8c1791d83ea03d89eacdba43caca95a611e4ae09
codoulingo1 <62474159+codoulingo1@users.noreply.github.com>
2020-04-02 13:52:33 +0300

    Added explantions.

--- app/src/main/java/com/example/coduolingo/MainActivity.java
+++ app/src/main/java/com/example/coduolingo/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(MainActivity.this, NonFreedum.class));
         if (j>1){
             try {
                 lessonCreator("57933", "Math", j);
--- app/src/main/java/com/example/coduolingo/NonFreedum.java
+++ app/src/main/java/com/example/coduolingo/NonFreedum.java
@@ -3,12 +3,28 @@ package com.example.coduolingo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Debug;
+import android.widget.Button;
 
 public class NonFreedum extends AppCompatActivity {
+
+    Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_non_freedum);
+        btn2 = (Button) findViewById(R.id.button2);
+        getLength();
     }
+
+    void getLength(){
+        //String btnText;
+        //btnText = btn2.getText().toString();
+        //btnText.length();
+
+        btn2.length();
+        Debug
+    }
+
 }
