0db99eb8c5ca357d8006f12633aa9e2ef23580cb
bobyo <bobyourhumanhelper@gmail.com>
2021-05-22 13:01:27 +0300

    תמונות פרופיל אוטומטיות ותיקון באגים (כפולים בליגות, יצירת משתמש עם גוגל)

---
+++ app/src/main/java/com/getcodly/codly/CustomAdapter.java
@@ -0,0 +1,104 @@
+package com.getcodly.codly;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.media.Image;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
+
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
+public class CustomAdapter extends BaseAdapter {
+    Context context;
+    ArrayList<String> names;
+    ArrayList<Integer> xp;
+    String n;
+    LayoutInflater inflter;
+
+    public CustomAdapter(Context applicationContext, ArrayList<String> names, ArrayList<Integer> xp, String n) {
+        this.context = context;
+        this.names = names;
+        this.xp = xp;
+        this.n = n;
+        inflter = (LayoutInflater.from(applicationContext));
+    }
+
+    @Override
+    public int getCount() {
+        return names.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view = inflter.inflate(R.layout.liga_list, null);
+        TextView name = (TextView) view.findViewById(R.id.aNametxt);
+        TextView xpp = (TextView) view.findViewById(R.id.aVersiontxt);
+        RelativeLayout l = (RelativeLayout) view.findViewById(R.id.view);
+        TextView num = (TextView) view.findViewById(R.id.num);
+        ImageView icon = (ImageView) view.findViewById(R.id.appIconIV);//
+        if (names.get(i).equals(n)){
+            SpannableStringBuilder builder = new SpannableStringBuilder();
+            SpannableString txtSpannable = new SpannableString(names.get(i));
+            StyleSpan boldSpan = new StyleSpan(Typeface.BOLD);
+            txtSpannable.setSpan(boldSpan, 0, names.get(i).length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            builder.append(txtSpannable);
+            name.setText(builder);
+        }
+        else {
+            name.setText(names.get(i));
+        }
+        Log.d("num", String.valueOf(i));
+        num.setText(String.valueOf(i + 1));
+        if (i <= 9){
+            l.setBackgroundColor(Color.parseColor("#98FB98"));
+        }
+        else if (i >= 20){
+            l.setBackgroundColor(Color.parseColor("#ffc6c4"));
+        }
+        else{
+            l.setBackgroundColor(Color.parseColor("#F5F5F5"));
+        }
+        xpp.setText("xp: " + String.valueOf(xp.get(i)));
+        String firstLetter;
+        if (names.get(i).split(" ").length == 1){
+            firstLetter = String.valueOf(names.get(i).split(" ")[0].charAt(0));
+        } else{
+            firstLetter = String.valueOf(names.get(i).split(" ")[0].charAt(0)) + String.valueOf(names.get(i).split(" ")[1].charAt(0));
+        }
+        ColorGenerator generator = ColorGenerator.MATERIAL;
+        int color1 = generator.getRandomColor();
+        Log.d(String.valueOf(color1), String.valueOf(color1));
+        TextDrawable drawable = TextDrawable.builder().beginConfig()
+                .width(60)  // width in px
+                .height(60) // height in px
+                .endConfig()
+                .buildRect(firstLetter, color1);
+        icon.setImageDrawable(drawable);
+        return view;
+    }
+}
