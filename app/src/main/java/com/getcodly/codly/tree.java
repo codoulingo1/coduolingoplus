1b4ca99dfa493c19b959f932b50fa7c1d40ad7f3
bobyo <bobyourhumanhelper@gmail.com>
2021-06-05 13:07:56 +0300

    תמונות פרופיל אוטומטיות ותיקון באגים (כפולים בליגות, יצירת משתמש עם גוגל)

--- app/src/main/java/com/getcodly/codly/tree.java
+++ app/src/main/java/com/getcodly/codly/tree.java
@@ -183,7 +183,7 @@ public class tree extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mainScreen.inLessonName = "מבוא לפייתון";
-                startLesson(Arrays.asList("10-10-10~נדבגוי", "1-1-1~מבוא לפייתון"), Arrays.asList(""));
+                startLesson(Arrays.asList("1-1-2~הקוד הראשון שלי", "1-1-1~מבוא לפייתון"), Arrays.asList(""));
             } //savta
         });
         skill2.setOnClickListener(new View.OnClickListener() {
